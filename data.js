80ffe1dcb146566c1705f0161ee7229567c3be83
gabridim18-lab <gabridim18@gmail.com>
2026-05-20 20:55:40 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,208 +1,189 @@
 const accommodations = [
 
+   {
+    id: "on_the_rock_sky_view_suite",
+    beachSlug: "athytos-beach",
+    flyer: "on-the-rock-sky-view-suite.webp"
+  },
   {
-  id: "on_the_rock_sky_view_suite",
-
-  flyer:
-    "on-the-rock-sky-view-suite.webp"
-},
-{
-  id: "marevia_beachfront_apartment_pefkohori",
-
-  flyer:
-    "marevia-beachfront-apartment-pefkohori.webp"
-},
-{
-  id: "iconic_olea_villa_pefkohori_with_pool_beach",
-
-  flyer:
-    "iconic-olea-villa-pefkohori-with-pool-beach.webp"
-},
-{
-  id: "big_studio_cc_in_mola_kaliva",
-
-  flyer:
-    "big-studio-cc-in-mola-kaliva.webp"
-},
-{
-  id: "infinity_view_villa_in_siviri",
-
-  flyer:
-    "infinity-view-villa-in-siviri.webp"
-},
-{
-  id: "polichrono_summer_escape",
-
-  flyer:
-    "polichrono-summer-escape.webp"
-},
-{
-  id: "prive_villa_in_mola_kaliva",
-
-  flyer:
-    "prive-villa-in-mola-kaliva.webp"
-},
-{
-  id: "sea_view_apartment_in_agia_triada",
-
-  flyer:
-    "sea-view-apartment-in-agia-triada.webp"
-},
-{
-  id: "avra_k7_villa_in_possidi_west_beach",
-
-  flyer:
-    "avra-k7-villa-in-possidi-west-beach.webp"
-},
-
-    {
-    id: "big_app_trikorfo",
-
-    flyer:
-      "3-bedroom-tricorfo.webp"
+    id: "marevia_beachfront_apartment_pefkohori",
+    beachSlug: "paralia-pefkochori",
+    flyer: "marevia-beachfront-apartment-pefkohori.webp"
+  },
+  {
+    id: "iconic_olea_villa_pefkohori_with_pool_beach",
+    beachSlug: "pefkochori-beach",
+    flyer: "iconic-olea-villa-pefkohori-with-pool-beach.webp"
+  },
+  {
+    id: "big_studio_cc_in_mola_kaliva",
+    beachSlug: "mola-kaliva",
+    flyer: "big-studio-cc-in-mola-kaliva.webp"
+  },
+  {
+    id: "infinity_view_villa_in_siviri",
+    beachSlug: "siviri",
+    flyer: "infinity-view-villa-in-siviri.webp"
+  },
+  {
+    id: "polichrono_summer_escape",
+    beachSlug: "polychrono-beach",
+    flyer: "polichrono-summer-escape.webp"
+  },
+  {
+    id: "prive_villa_in_mola_kaliva",
+    beachSlug: "mola-kaliva",
+    flyer: "prive-villa-in-mola-kaliva.webp"
+  },
+  {
+    id: "sea_view_apartment_in_agia_triada",
+    beachSlug: "agia-triada-beach",
+    flyer: "sea-view-apartment-in-agia-triada.webp"
+  },
+  {
+    id: "avra_k7_villa_in_possidi_west_beach",
+    beachSlug: "possidi-west-beach",
+    flyer: "avra-k7-villa-in-possidi-west-beach.webp"
   },
 
   {
-  id: "aya_app_kallithea",
+    id: "big_app_trikorfo",
+    beachSlug: "trikorfo-beach",
+    flyer: "3-bedroom-tricorfo.webp"
+  },
 
-  flyer:
-    "aya-app-kallithea.webp"
-},
   {
-  id: "artes_olive_haven_apartament_in_skala_fourkas",
-
-  flyer:
-    "artes-olive-haven-apartament-in-skala-fourkas.webp"
-},
+    id: "aya_app_kallithea",
+    beachSlug: "kalithea-beach",
+    flyer: "aya-app-kallithea.webp"
+  },
+  {
+    id: "artes_olive_haven_apartament_in_skala_fourkas",
+    beachSlug: "skala-fourkas-beach",
+    flyer: "artes-olive-haven-apartament-in-skala-fourkas.webp"
+  },
   {
     id: "amazing_view_by_the_pool",
-
-    flyer:
-      "amazing-view-by-the-pool.webp"
+    beachSlug: "trani-ammouda",
+    flyer: "amazing-view-by-the-pool.webp"
   },
 
   {
     id: "amira_house_sykia",
-
-    flyer:
-      "amira-house-sykia.webp"
+    beachSlug: "sykias-beach",
+    flyer: "amira-house-sykia.webp"
   },
   {
-  id: "angelika_app_polychrono",
+    id: "angelika_app_polychrono",
+    beachSlug: "polychrono-beach",
+    flyer: "angelika-app-polychrono.webp"
+  },
+  {
+    id: "alelia_app_lagonisi",
+    beachSlug: "lagonisi-beach",
+    flyer: "alelia-app-lagonisi.webp"
+  },
+  {
+    id: "apartament_skala_fourka",
+    beachSlug: "skala-fourkas-beach",
+    flyer: "apartament-skala-fourka.webp"
+  },
+  {
+    id: "beach_front_apartment_in_pefkochori",
+    beachSlug: "pefkochori-beach",
+    flyer: "beach-front-apartment-in-pefkochori.webp"
+  },
+  {
+    id: "beach_app_pefkochori",
+    beachSlug: "pefkochori-beach",
+    flyer: "beach-app-pefkochori.webp"
+  },
+  {
+    id: "app_mola_kaliva",
+    beachSlug: "mola-kaliva",
+    flyer: "app-mola-kaliva.webp"
+  },
+  {
+    id: "appartament_for_5_in_agia_paraskevi",
+    beachSlug: "loutra-agias-paraskevis-beach",
+    flyer: "appartament-for-5-in-agia-paraskevi.webp"
+  },
+  {
+    id: "big_villa_in_kriopigi",
+    beachSlug: "kryopigi-beach",
+    flyer: "big-villa-in-kriopigi.webp"
+  },
+  {
+    id: "big_villa_in_nea_potidea_with_7_bedrooms",
+    beachSlug: "paralia-nea-potidea",
+    flyer: "big-villa-in-nea-potidea-with-7-bedrooms.webp"
+  },
+  {
+    id: "blue_breeze_sykia",
+    beachSlug: "sykias-beach",
+    flyer: "blue-breeze-sykia.webp"
+  },
+  {
+    id: "cassa_luca_in_glarokavmos",
+    beachSlug: "glarokavos-beach",
+    flyer: "cassa-luca-in-glarokavmos.webp"
+  },
+  {
+    id: "cozy_house_in_kallithea",
+    beachSlug: "kalithea-beach",
+    flyer: "cozy-house-in-kallithea.webp"
+  },
+  {
+    id: "detached_house_pefkochori",
+    beachSlug: "pefkochori-beach",
+    flyer: "detached-house-pefkochori.webp"
+  },
+  {
+    id: "double_room_in_agia_paraskevi",
+    beachSlug: "loutra-agias-paraskevis-beach",
+    flyer: "double-room-in-agia-paraskevi.webp"
+  },
 
-  flyer:
-    "angelika-app-polychrono.webp"
-},
-{
-  id: "alelia_app_lagonisi",
+  {
+    id: "ladadika_luxury_studios_1",
+    beachSlug: "thessaloniki",
+    flyer: "ladadika-luxury-studios-1.webp"
+  },
+  {
+    id: "double_room_in_loutra_beach",
+    beachSlug: "loutra-agias-paraskevis-beach",
+    flyer: "double-room-in-loutra-beach.webp"
+  },
+  {
+    id: "double_room_in_skala_fourka",
+    beachSlug: "skala-fourkas-beach",
+    flyer: "double-room-in-skala-fourka.webp"
+  },
+  {
+    id: "electra_beach_appartment_skioni",
+    beachSlug: "nea-skioni-beach",
+    flyer: "electra-beach-appartment-skioni.webp"
+  },
+  {
+    id: "elena_maisonette_nea_plagia",
+    beachSlug: "nea-plagia-beach",
+    flyer: "elena-maisonette-nea-plagia.webp"
+  },
+  {
+    id: "elia_k3_villa_in_possidi_west_beach",
+    beachSlug: "possidi-west-beach",
+    flyer: "elia-k3-villa-in-possidi-west-beach.webp"
+  },
+  {
+    id: "toumpa_cozy_apartment_in_thessaloniki",
+    beachSlug: "nea-krini-beach",
+    flyer: "toumpa-cozy-apartment-in-thessaloniki.webp"
+  },
+  {
+  id: "kate_app_perea",
+  beachSlug: "perea-beach",
+  flyer: "kate-app-perea.webp"
+}
 
-  flyer:
-    "alelia-app-lagonisi.webp"
-},
-{
-  id: "apartament_skala_fourka",
-
-  flyer:
-    "apartament-skala-fourka.webp"
-},
-{
-  id: "beach_front_apartment_in_pefkochori",
-
-  flyer:
-    "beach-front-apartment-in-pefkochori.webp"
-},
-{
-  id: "beach_app_pefkochori",
-
-  flyer:
-    "beach-app-pefkochori.webp"
-},
-{
-  id: "app_mola_kaliva",
-
-  flyer:
-    "app-mola-kaliva.webp"
-},
-{
-  id: "appartament_for_5_in_agia_paraskevi",
-
-  flyer:
-    "appartament-for-5-in-agia-paraskevi.webp"
-},
-{
-  id: "big_villa_in_kriopigi",
-
-  flyer:
-    "big-villa-in-kriopigi.webp"
-},
-{
-  id: "big_villa_in_nea_potidea_with_7_bedrooms",
-
-  flyer:
-    "big-villa-in-nea-potidea-with-7-bedrooms.webp"
-},
-{
-  id: "blue_breeze_sykia",
-
-  flyer:
-    "blue-breeze-sykia.webp"
-},
-{
-  id: "cassa_luca_in_glarokavmos",
-
-  flyer:
-    "cassa-luca-in-glarokavmos.webp"
-},
-{
-  id: "cozy_house_in_kallithea",
-
-  flyer:
-    "cozy-house-in-kallithea.webp"
-},
-{
-  id: "detached_house_pefkochori",
-
-  flyer:
-    "detached-house-pefkochori.webp"
-},
-{
-  id: "double_room_in_agia_paraskevi",
-
-  flyer:
-    "double-room-in-agia-paraskevi.webp"
-},
-
-{
-  id: "ladadika_luxury_studios_1",
-beachSlug:
-    "thessaloniki",
-  flyer:
-    "ladadika-luxury-studios-1.webp"
-},
-{
-  id: "double_room_in_loutra_beach",
-  beachSlug: "loutra_agias_paraskevis_beach",
-  flyer: "double-room-in-loutra-beach.webp"
-},
-{
-  id: "double_room_in_skala_fourka",
-  beachSlug: "skala_fourkas_beach",
-  flyer: "double-room-in-skala-fourka.webp"
-},
-{
-  id: "electra_beach_appartment_skioni",
-  beachSlug: "nea_skioni_beach",
-  flyer: "electra-beach-appartment-skioni.webp"
-},
-{
-  id: "elena_maisonette_nea_plagia",
-  beachSlug: "nea_plagia_beach",
-  flyer: "elena-maisonette-nea-plagia.webp"
-},
-{
-  id: "elia_k3_villa_in_possidi_west_beach",
-  beachSlug: "possidi_west_beach",
-  flyer: "elia-k3-villa-in-possidi-west-beach.webp"
-},
 
 ];
